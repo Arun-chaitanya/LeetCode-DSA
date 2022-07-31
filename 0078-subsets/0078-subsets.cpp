66f06c528c8323828d0c0390b0abf248377314e2
AjgarDev <44457256+Arun-chaitanya@users.noreply.github.com>
2022-07-31 16:39:56 +0530

    Time: 19 ms (5.18%), Space: 18 MB (8.47%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums, int start = 0, vector<int> processed = {}) {
       
-      cout << start << " ";
       if(nums.size() == start){
         vector<vector<int>>vec1;
         vec1.push_back(processed);
@@ -10,8 +9,8 @@ public:
       }
       vector<int>added(processed);
       added.push_back(nums[start]);
-      vector<vector<int>>vec2 = subsets( nums, start+1, processed );
-      vector<vector<int>>vec1 = subsets( nums, start+1, added);
+      vector<vector<int>>vec1 = subsets( nums, start+1, processed );
+      vector<vector<int>>vec2 = subsets( nums, start+1, added);
       vec1.insert( vec1.end(), vec2.begin(), vec2.end() );
       return vec1;
     }
