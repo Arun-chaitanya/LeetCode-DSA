caf5a60be7ec13b7898a83b0c7ba796cfdeb1e61
AjgarDev <44457256+Arun-chaitanya@users.noreply.github.com>
2022-08-15 23:04:14 +0530

    Time: 132 ms (14.78%), Space: 44.8 MB (40.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -1,7 +1,3 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
 var subsetsWithDup = function(nums) {
   nums.sort(function(a, b){return a-b});
   var subFunc = function(numsArr, current=0, processed=[]) {
@@ -10,9 +6,7 @@ var subsetsWithDup = function(nums) {
   }
   let avoided = subFunc(numsArr, current+1, processed);
   let included = subFunc(numsArr, current+1, [...processed,numsArr[current]]);
-  
   return [...(numsArr[current] !== processed[processed.length - 1] ? avoided : []), ...included];
   }
   return subFunc(nums)
-  
 };
